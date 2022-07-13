84a7a2c17d61fa80e51d831a37a4512d858bf7ff
Carmen Valero <mvalero4@hotmail.es>
2022-07-13 08:50:02 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const modal = document.querySelector('.modal');
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+const close = document.querySelector('.modal-content-close');
+
+button1.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')
+})
+
+button2.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')
+})
+
+button3.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')
+})
+
+close.addEventListener('click', () => {
+    console.log('here')
+    modal.classList.add('hidden')
+    modal.classList.remove('visible')
+})
